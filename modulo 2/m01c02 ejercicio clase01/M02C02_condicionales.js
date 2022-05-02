01a2b0a60742ca230d60f20a95fa156eda0a05c6
Alexis Cozano <alexcozano@gmail.com>
2022-05-02 01:12:45 -0400

    ok

---
+++ modulo 2/m01c02 ejercicio clase01/M02C02_condicionales.js
@@ -0,0 +1,59 @@
+var nombreGato = "micifus";
+var numTelefono = 123123123;
+var mayorEdad = true;
+
+var edad = prompt("Ingrese su edad");  
+
+if(edad>=18){
+    alert("Ud. es mayor de edad");
+}else{
+    var edadFaltante=18-edad;
+    alert("Ud. es menor de edad, te faltan "+edadFaltante+" Años");
+   }
+   let edadFaltante=20;
+   var edad=0;
+
+   var numero1=5;
+   var numero2="5";
+
+   if(numero==numero2){
+       alert("son iguales (1)!!!");
+
+   }
+
+   if(numero1===numero2){
+       alert("son iguales (2)!!!")
+   }
+
+//multievaluacion
+
+var respuesta = prompt("cuanto es 1+1:A) 3 -B)5 - C)0 - d)2");
+if (respuesta="A{
+    alert("respuesta incorrecta");
+    }else if(respuesta="b"){
+        alert("respuesta incorrecta");
+    }else if(respuesta="C"){
+        alert("respuesta incorrecta");
+    }else if(respuesta="D"){
+        alert("respuesta correcta!!!!!!!");
+     }
+
+//desafio de las operaciones matemáticas
+var num1 = prompt("ingrese un número")
+var num2 = prompt("ingrese otro número")
+var operaciones=prompt(ingrese la operación);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
