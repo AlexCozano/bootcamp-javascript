299b06d1bb28c2a5d6cec7d3d47afb83f09ec1f2
Alexis Cozano <alexcozano@gmail.com>
2022-05-09 11:12:10 -0400

    lunes

--- modulo 3/js/M03C01_Clases.js
+++ modulo 3/js/M03C01_Clases.js
@@ -24,7 +24,7 @@ class Rectangulo{
     }
 }
       
-let fig1)= new Rectangulo(2,5);
+let fig1= new Rectangulo(2,5);
 //crear una clase que defina al usuario
 class Usuario{
     constructor(nombre,password){
@@ -37,10 +37,10 @@ var usuario1 = new Usuario("juanito","123");
 var usuario2 = new Usuario("pablito","333");
 var rectangulo1 = new rectangulo1(5,3)
 
-console.log("nombre del usuario (con punto): " + usuario1.nombre;
+console.log("nombre del usuario (con punto): " + usuario1.nombre);
 console.log("nombre del usuario (con corchete): " + usuario1["nombre"]);
 
 //agregar una propiedad a un usuario
 usuario1.activo=false;
 console.log(usuario1);
-console.log(usuario2);
+console.log(usuario2); 
