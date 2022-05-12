d488074529009802bd8ed6849f5b51c23cfc9ec1
Alexis Cozano <alexcozano@gmail.com>
2022-05-12 03:48:48 -0400

    gbn

--- modulo 2/js/M02C08_Eventos.js
+++ modulo 2/js/M02C08_Eventos.js
@@ -1,16 +1,28 @@
 function cambiarFondo(){
-    document.body.style.background=black;
-    document.body.style.color=white;
+    document.body.style.background="black;
+    document.body.style.color="white";
 }
 
 var boton = document.querySelector ("#boton1");
 
 //agregar la función al evento
 //primer parametro: texto indica el evento
-//boton.addEventListener("click",cambiarFondo());
 
-boton.addEventListener("clicl",function(){
+//forma noob
+//boton.addEventListener("click",cambiarFondo);
+
+//forma normal
+boton.addEventListener("click",
+    function(infoDelEvento){
+        document.body.style.backgroundColor="Black";
+        document.body.style.color=white;
+        console.log(infoDelEvento);
+    }
+)
+
+/* //forma avanzada
+boton.addEventListener("click",(e)=>{
     document.body.style.backgroundColor="Black";
     document.body.style.color=white;
-})
+}); */
 
