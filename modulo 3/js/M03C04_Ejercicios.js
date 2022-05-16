665829f3fb4d844954d3b72ba151d045b8e5f1e0
Alexis Cozano <alexcozano@gmail.com>
2022-05-16 05:22:18 -0400

    160520220522

---
+++ modulo 3/js/M03C04_Ejercicios.js
@@ -0,0 +1,17 @@
+//let foto = $("img");
+//let foto=document.querySelector("img");
+//let btn=$("#boton");
+let btn=document.querySelector("#boton");
+
+//console.log(foto);
+//console.log(btn);
+
+//agregar click
+btn.addEventListener("click",function(e){
+    $.getJSON("https://dog.ceo/api/breeds/image/random",function(data){
+        let obj=data;
+        let URLimagen=data.message;
+        let foto=document.querySelector("img");
+;        foto.setAttribute("src",URLimagen);
+    })
+})
