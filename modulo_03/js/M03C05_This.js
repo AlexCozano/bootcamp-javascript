5dae4129643a717b7f73ab582f689cea1caf08d0
Alexis Cozano <alexcozano@gmail.com>
2022-05-17 03:24:48 -0400

    070520220324

--- modulo_03/js/M03C05_This.js
+++ modulo_03/js/M03C05_This.js
@@ -1,17 +1,23 @@
-
 class Gato{
     constructor(nombre,raza){
         this.nombre;
         this.raza;
     }
-    saludar =function(){
-        console+.console.log("hola " + this.nombre);
+    saludarv2=function(saludo){
+        console.log(saludo +" " + this.nombre);
+        console.log(despedida +" " + this.nombre)
     }
+    saludarv3=function(saludos,despedida){
+        console.log(saludo +" " + this.nombre)
+    }
+
 }
 
-let gato1=new gato("neko","angora");
+let gato1=new Gato("neko","angora");
 
 gato1.nombre;
+gato1.saludo();
+gato1.saludarv2("que tal");
 this.nombre;
 
 //----------------------------------------------
@@ -20,5 +26,26 @@ let perro={
     nombre:"cachupin",
     raza:"terrier"
 }
+//bind
 let FuncionPrestada = gato1.saludar.bind(perro);
-perro.saludar();
+FuncionPrestada();
+//call
+gato1.saludar.call(perro);
+//call v2
+gato1.saludarv2.call(perro,"buenas tardes",);
+//call v2
+gato1.saludarv3.call(perro,"buenas tardes","nos vemos");
+//apply
+gato1.saludarv2.apply(["perro,[buenas que tal"]);
+gato1.saludarv2.apply(["perro,[buenas que tal","chao"]);
+
+//-----------------------------------------------
+
+function saludar(nombre){
+    return function(){
+        console.log(saludos + " "+ nombre);
+    }
+}
+
+let f=saludar("Denis")
+f("hola");
